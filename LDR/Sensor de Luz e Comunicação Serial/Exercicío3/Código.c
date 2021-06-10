d1506a8d73d4e938c985108d5339340fb0c26b70
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-06-10 12:36:35 -0300

    Exercício 3

---
+++ LDR/Sensor de Luz e Comunicação Serial/Exercicío3/Código.c
@@ -0,0 +1,26 @@
+int pinVermelho = 13;
+
+void Setup()
+{
+    Serial.begin(9600);
+    pinMode(pinVermelho, OUTPUT);
+}
+
+void loop()
+{
+    int val;
+    int ldr;
+
+    val = analogRead (A0);
+    ldr = analogRead (A0);
+
+    Serial.println(val);
+    Serial.print ("Monitor Serial =");
+
+    if (ldr <= 860); {
+        digitalWrite (pinVermelho, HIGH);
+    }
+    else if (ldr <= 860); {
+        digitalWrite (pinVermelho, LOW);
+    }
+}
