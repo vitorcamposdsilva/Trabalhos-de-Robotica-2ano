907449ddd5d857f908924f4345e4a5e087e633d9
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-06-15 18:50:18 -0300

    Update codigo.c

--- 2ºTrimestre/Potenciômetro/Exercício5/codigo.c
+++ 2ºTrimestre/Potenciômetro/Exercício5/codigo.c
@@ -1,1 +1,28 @@
-int pinVermelho = 
+int pinVermelho = 13;
+int pinVerde = 11;
+int pinAmarelo = 9;
+int pinAzul = 8;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    int val;
+    val = analogRead(A0);
+    Serial.println(val);
+    if (val >=200){
+        digitalWrite(pinVermelho, HIGH);
+    }
+    if (val >400){
+        digitalWrite(pinVerde, HIGH);
+    }
+    if (val >600){
+        digitalWrite(pinAmarelo, HIGH);
+    }
+    if (val >800){
+        digitalWrite(pinAzul, HIGH);
+    }
+}
