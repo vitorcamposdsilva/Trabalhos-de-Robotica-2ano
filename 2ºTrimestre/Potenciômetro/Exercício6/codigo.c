1eecbbc9b0f01d8982b7a07a3158647f3e925145
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-06-20 10:35:48 -0300

    Update codigo.c

--- 2ºTrimestre/Potenciômetro/Exercício6/codigo.c
+++ 2ºTrimestre/Potenciômetro/Exercício6/codigo.c
@@ -13,30 +13,57 @@ void loop()
     int val;
     val = analogRead(A0);
     Serial.println(val);
-    if (val >0){
+    if (val <200){
         digitalWrite(pinVermelho, HIGH);
         delay(2000);
         digitalWrite(pinVermelho, LOW);
         delay(6000); 
     }
-    if (val >0){
+    if (val <200){
         delay(250);
         digitalWrite(pinVerde, HIGH);
         delay(2000);
         digitalWrite(pinVerde, LOW);
         delay(4000); 
     }
-    if (val >0){
+    if (val <200){
         delay(500);
         digitalWrite(pinAmarelo, HIGH);
         delay(2000);
         digitalWrite(pinAmarelo, LOW);
         delay(2000);
     }
-    if (val >0){
+    if (val <200){
         delay(750);
         digitalWrite(pinAzul, HIGH);
         delay(2000);
+        digitalWrite(pinAzul, LOW);
+    }
+    // 2
+    if (val <200){
+        digitalWrite(pinVermelho, HIGH);
+        delay(2000);
+        digitalWrite(pinVermelho, LOW);
+        delay(4000); 
+    }
+    if (val >200){
+        delay(250);
+        digitalWrite(pinVerde, HIGH);
+        delay(1000);
+        digitalWrite(pinVerde, LOW);
+        delay(2000); 
+    }
+    if (val >200){
+        delay(500);
+        digitalWrite(pinAmarelo, HIGH);
+        delay(1000);
+        digitalWrite(pinAmarelo, LOW);
+        delay(1000);
+    }
+    if (val >200){
+        delay(750);
+        digitalWrite(pinAzul, HIGH);
+        delay(1000);
         digitalWrite(pinAzul, LOW);
     }
 }
