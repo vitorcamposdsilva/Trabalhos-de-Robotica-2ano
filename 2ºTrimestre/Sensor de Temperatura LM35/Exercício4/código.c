5d5f38836396aa92695796b037daf6a74338f334
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-07-12 08:23:45 -0300

    Update código.c

--- 2ºTrimestre/Sensor de Temperatura LM35/Exercício4/código.c
+++ 2ºTrimestre/Sensor de Temperatura LM35/Exercício4/código.c
@@ -1,9 +1,15 @@
+int buzzer = 13;
+float s = A0;
+float t = 0;
+
 void setup()
 {
-
+    Serial.begin(9600);
+    pinMode(buzzer, OUTPUT);
 }
 
 void loop()
 {
-    
+    t = map (((analogRead(s)-20)* 3.04) , 0 , 1023 , -40 , 125);
+    Serial.println(t);
 }
