1627449f50aace5f72313852012ae96f23ed8b4f
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-07-19 15:47:27 -0300

    Exercício

--- 2ºTrimestre/Sensor de Movimento PIR/Exercício2/codigo.c
+++ 2ºTrimestre/Sensor de Movimento PIR/Exercício2/codigo.c
@@ -1,10 +1,12 @@
 int pinVermelho = 4;
-int pinVerde = 8;
+int pinVerde = 6;
 
 void setup()
 {
     Serial.begin(9600);
     pinMode (2, INPUT);
+    pinMode (4, OUTPUT);
+    pinMode (6, OUTPUT);
 }
 
 void loop()
@@ -17,27 +19,12 @@ void loop()
     {
         Serial.println("ALERTA VERMELHO!!!");
         digitalWrite(pinVermelho, HIGH);
-        delay(2000);
+        digitalWrite(pinVerde, LOW);
     }
     if (sensor==LOW)
     {
-        Serial.println("ALERTA VERMELHO!!!");
+        Serial.println("tranquilo");
         digitalWrite(pinVermelho, LOW);
-        delay(2000);
-    }
-
-    if (sensor==LOW)
-    {
-        Serial.println("Tranquilo");
         digitalWrite(pinVerde, HIGH);
-        delay(2000);
-        digitalWrite(pinVerde, LOW);
-    }
-
-    if (sensor==HIGH)
-    {
-        Serial.println("Tranquilo");
-        digitalWrite(pinVerde, LOW);
-        delay(2000);
     }
 }
