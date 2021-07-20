23f0254d8a2cabdb6209edd2ae1d637ebf8d8a23
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-07-19 22:21:39 -0300

    Update código.c

--- 2ºTrimestre/Sensor de Movimento PIR/Exercício3/código.c
+++ 2ºTrimestre/Sensor de Movimento PIR/Exercício3/código.c
@@ -1,8 +1,30 @@
+int pinLaranja = 2;
+int pinVermelho = 4;
+int pinVerde = 6;
+int pinBranco = 13;
+
 void setup()
 {
-
+    Serial.begin(9600);
+    pinMode (12, INPUT);
+    pinMode (pinLaranja, OUTPUT);
+    pinMode (pinVermelho, OUTPUT);
+    pinMode (pinVerde, OUTPUT);
+    pinMode (pinBranco, OUTPUT);
 }
+
 void loop()
 {
-    
+    int sensor;
+
+    sensor = digitalRead(2);
+    Serial.println(sensor);
+    if (sensor==HIGH)
+    {
+        digitalWrite(pinLaranja, HIGH);
+        delay(800);
+        digitalWrite(pinLaranja,LOW);
+        delay(800);
+        digitalWrite(pinVermelho, HIGH);
+    }
 }
