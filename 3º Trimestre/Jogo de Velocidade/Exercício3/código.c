3fc65a35c65e7b5997637655990e2d1a858a9ac0
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-08-30 15:51:37 -0300

    exercício 3

---
+++ 3º Trimestre/Jogo de Velocidade/Exercício3/código.c
@@ -0,0 +1,27 @@
+int esquerdo = 8;
+int direito = 9;
+int bot_esquerdo = 3;
+int bot_direito = 2;
+void setup()
+{
+    pinMode(esquerdo, OUTPUT);
+    pinMode(direito, OUTPUT);
+    pinMode(bot_esquerdo, INPUT);
+    pinMode(bot_direito, INPUT);
+}
+void loop()
+{
+    if (digitalRead(bot_esquerdo) == HIGH){
+   	   digitalWrite(esquerdo, HIGH);
+    }
+    if (digitalRead(bot_esquerdo) == LOW){
+   	   digitalWrite(esquerdo, LOW);
+    }
+    //direito
+    if (digitalRead(bot_direito) == HIGH){
+   	   digitalWrite(direito, HIGH);
+    }
+    if (digitalRead(bot_direito) == LOW){
+   	   digitalWrite(direito, LOW);
+    }
+}
