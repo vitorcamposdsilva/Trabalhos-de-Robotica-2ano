6e132e149a5f8acaeb307764d586502564f5e2d1
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-10-16 11:29:01 -0300

    exercício 3

---
+++ 3º Trimestre/Ultrassom/código.c
@@ -0,0 +1,44 @@
+int pinTrigger = 3; 
+int pinEcho = 2; 
+float tempoEcho = 0;
+int pinVermelho = 8;
+const float velocidadeSom_mporus = 0.000340; 
+ 
+void setup(){
+  pinMode(pinTrigger, OUTPUT);
+  digitalWrite(pinTrigger, LOW);
+  pinMode(pinEcho, INPUT);
+  pinMode(pinVermelho, OUTPUT);
+ 
+  Serial.begin(9600);
+  delay(100);
+}
+
+void loop(){
+  float distancia;
+  
+  DisparaPulsoUltrassonico();
+  tempoEcho = pulseIn(pinEcho, HIGH);
+  distancia=CalculaDistancia(tempoEcho);
+  
+  Serial.print("Distancia em metros: ");
+  Serial.println(distancia);
+  
+  delay(2000);
+  if (distancia <= 0.20){
+    digitalWrite(pinVermelho, HIGH);
+  }
+  if (distancia > 0.20){
+    digitalWrite(pinVermelho, LOW);
+  }
+}
+
+void DisparaPulsoUltrassonico(){
+  digitalWrite(pinTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrigger, LOW);
+}
+
+float CalculaDistancia(float tempo_us){
+  return((tempo_us*velocidadeSom_mporus)/2);
+}
