8c393e500d5b42d8e67ca94db2ea123e780a99f7
vitorcamposdsilva <71096747+vitorcamposdsilva@users.noreply.github.com>
2021-10-30 12:56:57 -0300

    .

---
+++ 3º Trimestre/Display LCD 16x2/Exercício4/código.c
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int pin_bot=7;
+int val_bot;
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(2,1);
+}
+
+void loop() {
+  if(digitalRead(pin_bot) == HIGH){
+  delay(100);
+    lcd.clear();
+    lcd.print("LIGADO");
+    delay(1000);
+    lcd.clear();
+  }
+  if(digitalRead(pin_bot) == LOW){
+  lcd.setCursor(0, 0);
+  lcd.print("DESLIGADO");
+  }
+}
